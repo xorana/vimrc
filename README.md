3d85768011c09c4b02717727199fe6541eb5371a
xorana <7677151+xorana@users.noreply.github.com>
2017-11-02 13:56:03 +1100

    install script

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # vimrc
-my vimrc config.
-need the plugins in git submodules
+my vimrc config
+
+install by cloning and running the install script (it just links the files)
+	git clone https://github.com/xorana/vimrc.git ~/vimrc
+	sh ~/vimrc/install.sh
--- install.sh
+++ install.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 
-die() {
-	warn "$1"
-	exit 1
-}
-
 install() {
-	cd ~
-	git clone git@github.com:xorana/vimrc.git
-	cd vimrc
-
 	git submodule init
 	git submodule update
 
-	cd ~
 	ln -s ~/vimrc/vimrc ~/.vimrc
 	ln -s ~/vimrc/vim ~/.vim
+
+	mkdir -p ~/.vim/.tmp
+	mkdir -p ~/.vim/.undo
 
 	echo "xorana's vimrc installed!"
 }
